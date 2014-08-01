21bfb724d4f74272ef9c1f9e5a6daf63da0a1daa
DaaOtt <daaott@gmail.com>
2014-08-01 12:43:35 -0700

    Support for lower FOVs

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -11,10 +11,10 @@ function GM:CalcView(ply, pos, ang, fov, nearz, farz)
 			offset = ragoffset
 			tr.filter = ply:GetRagdollEntity()
 			tr.start = offset
-			tr.endpos = offset - ang:Forward() * 100
+			tr.endpos = offset - ang:Forward() * ((90 - fov) * 2.33 + 100)
 		elseif ply:Alive() then
 			tr.start = offset
-			tr.endpos = offset - ang:Forward() * 100
+			tr.endpos = offset - ang:Forward() * ((90 - fov) * 2.33 + 100)
 		end
 		tr.mask = MASK_BLOCKLOS
 		tr.mins = Vector(-nearz*2, -nearz*2, -nearz*2)
@@ -23,7 +23,7 @@ function GM:CalcView(ply, pos, ang, fov, nearz, farz)
 	if trace.Hit then
 		view.origin = trace.HitPos
 	else
-		view.origin = offset - ang:Forward() * 100
+		view.origin = offset - ang:Forward() * ((90 - fov) * 2.33 + 100)
 	end
 	if not IsValid(ply:GetRagdollEntity()) then
 		view.drawviewer = true

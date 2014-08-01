eaa4332b4638c1695a3ebe2e568a414b84b5846d
DaaOtt <daaott@gmail.com>
2014-08-01 00:29:36 -0700

    Neck snappage and function abstraction

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -7,6 +7,54 @@ include("shared.lua")
 include("sv_serversidebodies.lua")
 include("sv_puppetmaster.lua")
 
+function ExitRagdoll(ply, cmd)
+	ply:SetMoveType(MOVETYPE_WALK)
+	ply:SetCollisionGroup(COLLISION_GROUP_PLAYER)
+	ply:GetRagdollEntity():SetRenderBones(false)
+	timer.Simple(0.1, function()
+		if IsValid(ply:GetRagdollEntity()) then
+			local ragpos = ply:GetRagdollEntity():GetPos()
+			local trace = {}
+			local tr = {}
+				tr.filter = {ply:GetRagdollEntity(), ply}
+				tr.start = ragpos
+				tr.endpos = ragpos
+				tr.mins = Vector(-16, -16, 0)
+				tr.maxs = Vector(16, 16, 72)
+				tr.output = trace
+			for i = 1, 20 do
+				util.TraceHull(tr)
+				if trace.Hit then
+					local rand = Vector(math.random(-48, 48), math.random(-48, 48), math.random(-48, 48))
+					tr.start = ragpos + rand
+					tr.endpos = ragpos + rand
+				else
+					break
+				end
+			end
+			if trace.Hit then
+				ply:Kill()
+			end
+			ply:SetPos(trace.HitPos)
+			ply:SetVelocity(-ply:GetVelocity() + ply:GetRagdollEntity():GetVelocity())
+			ply:GetRagdollEntity():Remove()
+			ply:SetNoDraw(false)
+		end
+	end)
+end
+function EnterRagdoll(ply, cmd)
+	ply:CreateRagdoll()
+	ply:SetMoveType(MOVETYPE_NONE)
+	ply:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+	ply:SetNoDraw(true)
+	timer.Create("ragcreate", 0.1, 0, function()
+		if IsValid(ply:GetRagdollEntity()) then
+			ply:GetRagdollEntity():SetRenderBones(true)
+			timer.Destroy("ragcreate")
+		end
+	end)
+end
+
 function GM:PlayerLoadout(ply)
 	ply:SetModel("models/player/Group02/male_02.mdl")
 	ply:GodEnable()
@@ -14,54 +62,23 @@ end
 function GM:Move(ply, cmd)
 	if cmd:KeyReleased(IN_JUMP) then
 		if IsValid(ply:GetRagdollEntity()) then
-			ply:SetMoveType(MOVETYPE_WALK)
-			ply:SetCollisionGroup(COLLISION_GROUP_PLAYER)
-			ply:GetRagdollEntity():SetRenderBones(false)
-			timer.Simple(0.1, function()
-				if IsValid(ply:GetRagdollEntity()) then
-					local ragpos = ply:GetRagdollEntity():GetPos()
-					local trace = {}
-					local tr = {}
-						tr.filter = {ply:GetRagdollEntity(), ply}
-						tr.start = ragpos
-						tr.endpos = ragpos
-						tr.mins = Vector(-16, -16, 0)
-						tr.maxs = Vector(16, 16, 72)
-						tr.output = trace
-					for i = 1, 20 do
-						util.TraceHull(tr)
-						if trace.Hit then
-							local rand = Vector(math.random(-48, 48), math.random(-48, 48), math.random(-48, 48))
-							tr.start = ragpos + rand
-							tr.endpos = ragpos + rand
-						else
-							break
-						end
-					end
-					if trace.Hit then
-						ply:Kill()
-					end
-					ply:SetPos(trace.HitPos)
-					ply:SetVelocity(-ply:GetVelocity() + ply:GetRagdollEntity():GetVelocity())
-					ply:GetRagdollEntity():Remove()
-					ply:SetNoDraw(false)
-				end
-			end)
+			ExitRagdoll(ply, cmd)
 		elseif not ply:IsOnGround() then
-			ply:CreateRagdoll()
-			ply:SetMoveType(MOVETYPE_NONE)
-			ply:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
-			ply:SetNoDraw(true)
-			timer.Create("ragcreate", 0.1, 0, function()
-				if IsValid(ply:GetRagdollEntity()) then
-					ply:GetRagdollEntity():SetRenderBones(true)
-					timer.Destroy("ragcreate")
-				end
-			end)
+			EnterRagdoll(ply, cmd)
 		end
 	end
+
 	if cmd:KeyPressed(IN_USE) then
 		ply:SetMoveType(MOVETYPE_NOCLIP)
 	end
+
+	if IsValid(ply:GetRagdollEntity()) then
+		if ply:GetRagdollEntity().BoneDamage[10] > ply:GetRagdollEntity():GetNWInt("BreakPoint") then
+			ExitRagdoll(ply, cmd)
+		end
+	end
 end 
+function GM:GetFallDamage(ply, speed)
+	EnterRagdoll(ply)
+end
 

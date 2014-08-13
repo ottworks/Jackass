5e498d192098df7b1d0860128e5acc4952a07996
DaaOtt <daaott@gmail.com>
2014-08-13 14:48:55 -0700

    Hats!

--- gamemode/cl_hats.lua
+++ gamemode/cl_hats.lua
@@ -2,8 +2,11 @@ local model = ClientsideModel("models/stunt_helmet.mdl", RENDERGROUP_OPAQUE)
 model:SetModelScale(0.9, 0)
 local function drawhats()
 	for k, v in pairs(player.GetAll()) do
-		if v:GetNWString("Hat") == "Stunt Helmet" and not v:GetNoDraw() then
+		if v:GetNWString("Hat") == "Stunt Helmet" then
 			local pos, ang = LocalToWorld(Vector(1, 0, 0), Angle(0, -90, -90), v:GetBonePosition(6))
+			if v:GetNoDraw() and IsValid(v:GetRagdollEntity()) then
+				pos, ang = LocalToWorld(Vector(1, 0, 0), Angle(0, -90, -90), v:GetRagdollEntity():GetBonePosition(6))
+			end
 			model:SetRenderOrigin( pos );
 			model:SetRenderAngles( ang );
 			model:SetupBones();

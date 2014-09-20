c2abd86821e85529e3a25300d891576e3df32969
DaaOtt <daaott@gmail.com>
2014-09-20 12:15:12 -0700

    Doubled prop prices

--- gamemode/sh_buyables.lua
+++ gamemode/sh_buyables.lua
@@ -1,402 +1,306 @@
 BUYABLES = {
 	{
-		type = "prop_physics",
-		model = "models/props_borealis/bluebarrel001.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/oildrum001.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/canister_propane01a.mdl",
-		price = 300
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/concrete_barrier001a.mdl",
-		price = 300
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureBathtub001a.mdl",
-		price = 250,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_interiors/BathTub01a.mdl",
-		price = 250,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureTable002a.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_interiors/Furniture_chair01a.mdl",
-		price = 50
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/CinderBlock01a.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_trainstation/trashcan_indoor001b.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/tire001a_tractor.mdl",
-		price = 400
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/controlroom_desk001b.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/laundry_cart002.mdl",
-		price = 150,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/laundry_cart001.mdl",
-		price = 250,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/wood_fence02a.mdl",
-		price = 150,
-		offset = Angle(0, 90, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/wood_fence01a.mdl",
-		price = 250,
-		offset = Angle(0, 90, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/dockplank01b.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/kitchen_counter001a.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/kitchen_counter001b.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/laundry_basket001.mdl",
-		price = 100,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/cargo_container01b.mdl",
-		price = 5000
-	}, {
-		type = "prop_physics",
-		model = "models/props_lab/blastdoor001b.mdl",
-		price = 400
-	}, {
-		type = "prop_physics",
-		model = "models/props_lab/blastdoor001c.mdl",
-		price = 800
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/TrafficCone001a.mdl",
-		price = 20
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/PushCart01a.mdl",
-		price = 200,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/wood_pallet001a.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/props_trainstation/traincar_seats001.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/iBeam01a.mdl",
-		price = 300,
-		offset = Angle(0, 90, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/iBeam01a_cluster01.mdl",
-		price = 900,
-		offset = Angle(0, 90, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/MetalBucket02a.mdl",
-		price = 25
-	}, {
-		type = "prop_physics",
-		model = "models/props_trainstation/trainstation_post001.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/TrashDumpster02.mdl",
-		price = 3500,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/coolingtank02.mdl",
-		price = 6000
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/sawblade001a.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/car004a.mdl",
-		price = 3000,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/car005a.mdl",
-		price = 3000,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/car002a.mdl",
-		price = 3000,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/trailer001a.mdl",
-		price = 2000,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/wagon001a.mdl",
-		price = 500,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/van001a.mdl",
-		price = 3000,
-		material = "ice",
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_canal/boat002b.mdl",
-		price = 2000,
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/statue_horse.mdl",
-		price = 3500
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/watermelon01.mdl",
-		price = 10
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/metal_paintcan001b.mdl",
-		price = 10
-	}, {
-		type = "prop_physics",
-		model = "models/props_interiors/Radiator01a.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/prison_toilet01.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/playground_jungle_gym01a.mdl",
-		price = 300
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/playground_jungle_gym01b.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/playground_swingset01.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/playground_swingset_seat01a.mdl",
-		price = 20
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/pottery_large01a.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/TrapPropeller_Blade.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/TrapPropeller_Engine.mdl",
-		price = 250
-	}, {
-		type = "prop_physics",
-		model = "models/props_combine/breenchair.mdl",
-		price = 100,
-		material = "ice"
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureWashingmachine001a.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/playground_carousel01.mdl",
-		price = 500
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureFridge001a.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureCouch001a.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureCouch002a.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/fountain_01.mdl",
-		price = 2000
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/pulleyhook01.mdl",
-		price = 300
-	}, {
-		type = "prop_physics",
-		model = "models/props_junk/harpoon002a.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_interiors/VendingMachineSoda01a.mdl",
-		price = 250
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureDresser001a.mdl",
-		price = 150
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/interior_fence002d.mdl",
-		price = 200
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/construct/concrete_pipe01.mdl",
-		price = 300,
-		offset = Angle(90, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/construct/concrete_barrier00.mdl",
-		price = 250,
-		offset = Angle(0, 90, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/construct/concrete_barrier01.mdl",
-		price = 250,
-		offset = Angle(0, 90, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/misc/small_ramp.mdl",
-		price = 1000,
-		offset = Angle(0, 180, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/misc/iron_beam1.mdl",
-		price = 50,
-		offset = Angle(90, 0, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/misc/iron_beam2.mdl",
-		price = 100,
-		offset = Angle(90, 0, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/misc/iron_beam3.mdl",
-		price = 250,
-		offset = Angle(90, 0, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/props_phx/misc/iron_beam4.mdl",
-		price = 500,
-		offset = Angle(90, 0, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/hanging_stair_128.mdl",
-		price = 500,
-		offset = Angle(0, 90, 0)
-	}, {
-		type = "prop_physics",
-		model = "models/stairsupport_tall.mdl",
-		price = 100
-	}, {
-		type = "prop_physics",
-		model = "models/XQM/Rails/gumball_1.mdl",
-		price = 1000,
-	}, {
-		type = "prop_physics",
-		model = "models/XQM/Rails/trackball_1.mdl",
-		price = 1000,
-	}, {
-		type = "prop_physics",
-		model = "models/XQM/CoasterTrack/train_2.mdl",
-		price = 8000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/playgroundTick-tack-toe_post01.mdl",
-		price = 2000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/FurnitureBed001a.mdl",
-		price = 1000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_wasteland/prison_bedframe001b.mdl",
-		price = 1000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/column02a.mdl",
-		price = 5000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_combine/combine_train02b.mdl",
-		price = 10000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/car001a_hatchback.mdl",
-		price = 5000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/trailer002a.mdl",
-		price = 5000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_vehicles/truck001a.mdl",
-		price = 6000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_combine/combinetower001.mdl",
-		price = 10000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/playgroundslide01.mdl",
-		price = 1000,
-	}, {
-		type = "prop_physics",
-		model = "models/props_c17/oildrum001_explosive.mdl",
-		price = 1000,
-	},
+	 	type = "prop_physics",
+ 		model = "models/props_borealis/bluebarrel001.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/oildrum001.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/canister_propane01a.mdl",
+ 		price = 600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/concrete_barrier001a.mdl",
+ 		price = 600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/FurnitureBathtub001a.mdl",
+ 		price = 500
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_interiors/BathTub01a.mdl",
+ 		price = 500
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/FurnitureTable002a.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_interiors/Furniture_chair01a.mdl",
+ 		price = 100
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/CinderBlock01a.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_trainstation/trashcan_indoor001b.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_vehicles/tire001a_tractor.mdl",
+ 		price = 800
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/controlroom_desk001b.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/laundry_cart002.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/laundry_cart001.mdl",
+ 		price = 500
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/wood_fence02a.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/wood_fence01a.mdl",
+ 		price = 500
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/dockplank01b.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/kitchen_counter001a.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/kitchen_counter001b.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/laundry_basket001.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/cargo_container01b.mdl",
+ 		price = 10000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_lab/blastdoor001b.mdl",
+ 		price = 800
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_lab/blastdoor001c.mdl",
+ 		price = 1600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/TrafficCone001a.mdl",
+ 		price = 40
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/PushCart01a.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/wood_pallet001a.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_trainstation/traincar_seats001.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/iBeam01a.mdl",
+ 		price = 600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/iBeam01a_cluster01.mdl",
+ 		price = 1800
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/MetalBucket02a.mdl",
+ 		price = 50
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_trainstation/trainstation_post001.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/TrashDumpster02.mdl",
+ 		price = 7000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/coolingtank02.mdl",
+ 		price = 12000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/sawblade001a.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_vehicles/car004a_physics.mdl",
+ 		price = 6000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_vehicles/car005a_physics.mdl",
+ 		price = 6000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_vehicles/car002a_physics.mdl",
+ 		price = 6000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_vehicles/trailer001a.mdl",
+ 		price = 4000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_vehicles/wagon001a_phy.mdl",
+ 		price = 500
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_vehicles/van001a_physics.mdl",
+ 		price = 6000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_canal/boat002b.mdl",
+ 		price = 4000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/statue_horse.mdl",
+ 		price = 7000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/watermelon01.mdl",
+ 		price = 20
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/metal_paintcan001b.mdl",
+ 		price = 20
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_interiors/Radiator01a.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/prison_toilet01.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/playground_jungle_gym01a.mdl",
+ 		price = 600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/playground_jungle_gym01b.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/playground_swingset01.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/playground_swingset_seat01a.mdl",
+ 		price = 40
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/pottery_large01a.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/TrapPropeller_Blade.mdl",
+ 		price = 300
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/TrapPropeller_Engine.mdl",
+ 		price = 500
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_combine/breenchair.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/FurnitureWashingmachine001a.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/playground_carousel01.mdl",
+ 		price = 1000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/FurnitureFridge001a.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/FurnitureCouch001a.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/FurnitureCouch002a.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/fountain_01.mdl",
+ 		price = 4000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/pulleyhook01.mdl",
+ 		price = 600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_junk/harpoon002a.mdl",
+ 		price = 600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_interiors/VendingMachineSoda01a.mdl",
+ 		price = 1400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_c17/FurnitureDresser001a.mdl",
+ 		price = 600
+ 	}, {
+				
+ 		type = "prop_physics",
+ 		model = "models/props_wasteland/interior_fence002d.mdl",
+ 		price = 800
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/construct/concrete_pipe01.mdl",
+ 		price = 600
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/construct/concrete_barrier00.mdl",
+ 		price = 1000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/construct/concrete_barrier01.mdl",
+ 		price = 1000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/misc/small_ramp.mdl",
+ 		price = 4000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/misc/iron_beam1.mdl",
+ 		price = 200
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/misc/iron_beam2.mdl",
+ 		price = 400
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/misc/iron_beam3.mdl",
+ 		price = 1000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_phx/misc/iron_beam4.mdl",
+ 		price = 2000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props_bts/hanging_stair_128.mdl",
+ 		price = 2000
+ 	}, {
+ 		type = "prop_physics",
+ 		model = "models/props/de_vertigo/stairsupport_tall.mdl",
+ 		price = 400
+ 	},
 }
 table.sort(BUYABLES, function(a, b)
 	local mdl = a.model

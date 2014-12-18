217cf871b4a90e48634582abbe09e946d79e82e3
DaaOtt <daaott@gmail.com>
2014-12-17 16:45:06 -0800

    fixed hats drawing at origin

--- gamemode/cl_hats.lua
+++ gamemode/cl_hats.lua
@@ -1,8 +1,10 @@
 local model = ClientsideModel("models/stunt_helmet.mdl", RENDERGROUP_OPAQUE)
 model:SetModelScale(0.9, 0)
+model:SetNoDraw(true)
 local function drawhats()
 	for k, v in pairs(player.GetAll()) do
 		if v:GetNWString("Hat") == "Stunt Helmet" then
+			model:SetNoDraw(false)
 			local pos, ang = LocalToWorld(Vector(1, 0, 0), Angle(0, -90, -90), v:GetBonePosition(6))
 			if IsValid(v:GetRagdollEntity()) then
 				pos, ang = LocalToWorld(Vector(1, 0, 0), Angle(0, -90, -90), v:GetRagdollEntity():GetBonePosition(6))
